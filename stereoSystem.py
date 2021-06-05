a01b970e6a408a661ddbc4534566b11a73e9a2b5
Tristan Marshall <tlsmarshall1@gmail.com>
2021-06-04 21:57:39 -0400

    Code State at final submission

--- stereoSystem.py
+++ stereoSystem.py
@@ -417,24 +417,24 @@ if __name__ == "__main__":
     
 
 
-dirname = os.path.dirname(__file__)
-imagLpath = dirname + "\\MiddEval3-data-Q\\MiddEval3\\trainingQ\\Pipes\\im0.png"
-imagRpath = dirname + "\\MiddEval3-data-Q\\MiddEval3\\trainingQ\\Pipes\\im1.png"
-cam0=np.array([[989.886, 0, 392.942], [0, 989.886, 243.221], [0, 0, 1,]])
-cam1=np.array([[989.886, 0, 412.274], [0, 989.886, 243.221], [0, 0, 1]])
-width=735
-height=485
-doffs=19.331
-baseline=236.922
+# dirname = os.path.dirname(__file__)
+# imagLpath = dirname + "\\MiddEval3-data-Q\\MiddEval3\\trainingQ\\Pipes\\im0.png"
+# imagRpath = dirname + "\\MiddEval3-data-Q\\MiddEval3\\trainingQ\\Pipes\\im1.png"
+# cam0=np.array([[989.886, 0, 392.942], [0, 989.886, 243.221], [0, 0, 1,]])
+# cam1=np.array([[989.886, 0, 412.274], [0, 989.886, 243.221], [0, 0, 1]])
+# width=735
+# height=485
+# doffs=19.331
+# baseline=236.922
 
-s2 = stereoSystem(imagLpath, imagRpath, cam0, cam1, height, width, doffs, baseline)
-# s2.drawEpipolar(100,200)
-# s2.findFundMatr()
-# corresPoint = s.findCorrespondant(200,200, 101)
-# s2.displayCorrespondent(150,150, 27)
-# s2.displayCorrespondent(250,350, 27)
-# s2.displayCorrespondent(350,250, 27)
-# s2.displayCorrespondent(365,241, 27)
-# s2.displayCorrespondent(600,50, 27)
-t = time.time()
-s.generateDispMap(23)
+# s2 = stereoSystem(imagLpath, imagRpath, cam0, cam1, height, width, doffs, baseline)
+# # s2.drawEpipolar(100,200)
+# # s2.findFundMatr()
+# # corresPoint = s.findCorrespondant(200,200, 101)
+# # s2.displayCorrespondent(150,150, 27)
+# # s2.displayCorrespondent(250,350, 27)
+# # s2.displayCorrespondent(350,250, 27)
+# # s2.displayCorrespondent(365,241, 27)
+# # s2.displayCorrespondent(600,50, 27)
+# t = time.time()
+# s2.generateDispMap(23)
